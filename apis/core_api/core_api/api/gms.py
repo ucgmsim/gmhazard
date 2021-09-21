c80216cfe0e49b12fd0cedba9705b2c0a0ae6357
Tom Son <sdw123123@gmail.com>
2021-09-22 09:54:40 +1200

    word update

--- apis/core_api/core_api/api/gms.py
+++ apis/core_api/core_api/api/gms.py
@@ -182,7 +182,7 @@ def download_gms_results(token):
         with tempfile.TemporaryDirectory() as tmp_dir:
             # TODO: Find a way to deal with disaggregation data with Core API
             # For instance, Projects already come with disaggregation data
-            # whereas, Core API, disagg data needs to be computed
+            # whereas, Core API, disaggregation data needs to be computed
             zip_ffp, _ = su.api.create_gms_download_zip(
                 cached_data.gms_result,
                 tmp_dir,
