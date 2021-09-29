e50c35dfe41594297245c69b34a15ea809a4f624
Tom Son <sdw123123@gmail.com>
2021-09-30 12:41:59 +1300

    rename logo to gmhazard

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -9,84 +9,83 @@ import { GlobalContext } from "context";
 import { LogoutButton, LoginButton } from "components/NavBar";
 
 import "assets/style/NavBar.css";
-// Update the logo when we have a newer one
-import GMHazardLogo from "assets/seistech_long_logo_simple.png";
+import GMHazardLogo from "assets/gmhazard_long_logo_simple.png";
 
 const MainNav = () => {
-  const { hasPermission } = useContext(GlobalContext);
+	const { hasPermission } = useContext(GlobalContext);
 
-  return (
-    <Nav className="mr-auto" navbar>
-      <NavLink
-        tag={RouterNavLink}
-        to="/"
-        exact
-        activeClassName="router-link-exact-active"
-      >
-        Home
-      </NavLink>
-      {hasPermission("hazard") ? (
-        <NavLink
-          tag={RouterNavLink}
-          to="/hazard"
-          exact
-          activeClassName="router-link-exact-active"
-        >
-          Hazard Analysis
-        </NavLink>
-      ) : null}
+	return (
+		<Nav className="mr-auto" navbar>
+			<NavLink
+				tag={RouterNavLink}
+				to="/"
+				exact
+				activeClassName="router-link-exact-active"
+			>
+				Home
+			</NavLink>
+			{hasPermission("hazard") ? (
+				<NavLink
+					tag={RouterNavLink}
+					to="/hazard"
+					exact
+					activeClassName="router-link-exact-active"
+				>
+					Hazard Analysis
+				</NavLink>
+			) : null}
 
-      {hasPermission("project") ? (
-        <NavLink
-          tag={RouterNavLink}
-          to="/project"
-          exact
-          activeClassName="router-link-exact-active"
-        >
-          Projects
-        </NavLink>
-      ) : null}
+			{hasPermission("project") ? (
+				<NavLink
+					tag={RouterNavLink}
+					to="/project"
+					exact
+					activeClassName="router-link-exact-active"
+				>
+					Projects
+				</NavLink>
+			) : null}
 
-      <NavLink
-        tag={RouterNavLink}
-        to="/framework-docs"
-        exact
-        activeClassName="router-link-exact-active"
-      >
-        Framework Documents
-      </NavLink>
-    </Nav>
-  );
+			<NavLink
+				tag={RouterNavLink}
+				to="/framework-docs"
+				exact
+				activeClassName="router-link-exact-active"
+			>
+				Framework Documents
+			</NavLink>
+		</Nav>
+	);
 };
 
 const AuthNav = () => {
-  const { isAuthenticated } = useAuth0();
-  return (
-    <Nav className="justify-content-end">
-      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
-    </Nav>
-  );
+	const { isAuthenticated } = useAuth0();
+	return (
+		<Nav className="justify-content-end">
+			{isAuthenticated ? <LogoutButton /> : <LoginButton />}
+		</Nav>
+	);
 };
 
 const NavBar = () => {
-  return (
-    <Navbar color="light" light expand="md" bg="dark">
-      <div className="container-fluid max-width">
-        <NavbarBrand tag={Link} to={"/"}>
-          <img
-            src={GMHazardLogo}
-            alt=""
-            width="150"
-            className="nav-brand-logo"
-          />
-        </NavbarBrand>
+	return (
+		<Navbar color="light" light expand="md" bg="dark">
+			<div className="container-fluid max-width">
+				<NavbarBrand tag={Link} to={"/"}>
+					<img
+						src={GMHazardLogo}
+						alt=""
+						width="150"
+						className="nav-brand-logo"
+					/>
+				</NavbarBrand>
 
-        <MainNav />
+				<MainNav />
 
-        <AuthNav />
-      </div>
-    </Navbar>
-  );
+				<AuthNav />
+			</div>
+		</Navbar>
+	);
 };
 
 export default NavBar;
