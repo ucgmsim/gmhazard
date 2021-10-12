77ae0acb87be087da9e65f74a57c2b7625cebc63
sdw123123@gmail.com <sdw123123@gmail.com>
2021-10-13 10:25:53 +1300

    disable create a new projects

--- tools/project_gen/project_gen/scripts/run_project_creation.py
+++ tools/project_gen/project_gen/scripts/run_project_creation.py
@@ -31,7 +31,7 @@ def main(
         projects_base_dir,
         scripts_dir,
         n_procs=n_procs,
-        new_project=True,
+        new_project=False,
         erf_dir=erf_dir,
         erf_pert_dir=erf_pert_dir,
         flt_erf_version=flt_erf_version,
@@ -66,6 +66,13 @@ if __name__ == "__main__":
         help="Number of processes to use - minimum is 2",
         default=2,
     )
+    # Discuss with team to add this parser, disable it for now
+    # parser.add_argument(
+    #     "--disable_new_project",
+    #     type=bool,
+    #     help="True for completely new project, False for only the results are computed",
+    #     default=True,
+    # )
     parser.add_argument(
         "--erf_dir", type=Path, help="Path to the ERF directory", default=None
     )
