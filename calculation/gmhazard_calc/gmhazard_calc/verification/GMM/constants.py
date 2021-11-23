ba7b34368b838cbef927355946a517aa3423091e
sdw123123@gmail.com <sdw123123@gmail.com>
2021-11-23 14:59:01 +1300

    Update models based on the latest version of empirical

--- calculation/gmhazard_calc/gmhazard_calc/verification/GMM/constants.py
+++ calculation/gmhazard_calc/gmhazard_calc/verification/GMM/constants.py
@@ -26,20 +26,8 @@ CONST_SITE_PARAMS = {
 }
 
 CONST_FAULT_PARAMS = {
-    "ACTIVE_SHALLOW": {
-        "rake": 0,
-        "dip": 90,
-        "ztor": 0,
-        "hdepth": 10,
-        "zbot": 11,
-    },
-    "SUBDUCTION_SLAB": {
-        "rake": -90,
-        "dip": 45,
-        "ztor": 50,
-        "hdepth": 60,
-        "zbot": 70,
-    },
+    "ACTIVE_SHALLOW": {"rake": 0, "dip": 90, "ztor": 0, "hdepth": 10, "zbot": 11,},
+    "SUBDUCTION_SLAB": {"rake": -90, "dip": 45, "ztor": 50, "hdepth": 60, "zbot": 70,},
     "SUBDUCTION_INTERFACE": {
         "rake": 90,
         "dip": 20,
@@ -52,64 +40,42 @@ CONST_FAULT_PARAMS = {
 # Just for psha_psa_plots.py
 PSA_IM_NAME = "pSA"
 
-# Based on empirical_db/empirical_model_configs/20p11.yaml
+# Based on db_creation/empirical_db/empirical_model_configs/21p10.yaml
 ss_spectral_models = [
     "ZA_06",
-    "A_18",
     "BCH_16",
-    "P_20_SS",
-    "HA_20_SS",
-    "PH_20_SS",
-    "CH_20_SS",
-    "AG_20_SS",
-    "AG_20_SS_NZ",
-    "K_20_SS",
-    "K_20_SS_NZ",
-    "Si_20_SS",
-    "Z_16_SS",
+    "A_18",
+    "P_20",
+    "AG_20",
+    "AG_20_NZ",
+    "K_20",
+    "K_20_NZ",
 ]
 si_spectral_models = [
     "ZA_06",
-    "A_18",
     "BCH_16",
-    "P_20_SI",
-    "HA_20_SI",
-    "PH_20_SI",
-    "CH_20_SI",
-    "AG_20_SI",
-    "AG_20_SI_NZ",
-    "K_20_SI",
-    "K_20_SI_NZ",
-    "Si_20_SI",
-    "Z_16_SI",
+    "A_18",
+    "P_20",
+    "AG_20",
+    "AG_20_NZ",
+    "K_20",
+    "K_20_NZ",
 ]
 MODELS_DICT = {
     "ACTIVE_SHALLOW": {
         "PGV": ["Br_10", "ASK_14", "BSSA_14", "CB_14", "CY_14"],
         "PGA": ["Br_10", "ASK_14", "BSSA_14", "CB_14", "CY_14"],
         "pSA": ["Br_10", "ASK_14", "BSSA_14", "CB_14", "CY_14"],
-        "CAV": ["CB_10"],
-        "AI": ["CB_12"],
-        "Ds575": ["AS_16"],
-        "Ds595": ["AS_16"],
     },
     "SUBDUCTION_SLAB": {
-        "PGV": ["P_20_SS", "HA_20_SS", "CH_20_SS", "K_20_SS", "K_20_SS_NZ"],
+        "PGV": ["P_20", "K_20", "K_20_NZ"],
         "PGA": ss_spectral_models,
         "pSA": ss_spectral_models,
-        "CAV": ["CB_10"],
-        "AI": ["CB_12"],
-        "Ds575": ["AS_16"],
-        "Ds595": ["AS_16"],
     },
     "SUBDUCTION_INTERFACE": {
-        "PGV": ["P_20_SI", "HA_20_SI", "CH_20_SI", "K_20_SI", "K_20_SI_NZ"],
+        "PGV": ["P_20", "K_20", "K_20_NZ"],
         "PGA": si_spectral_models,
         "pSA": si_spectral_models,
-        "CAV": ["CB_10"],
-        "AI": ["CB_12"],
-        "Ds575": ["AS_16"],
-        "Ds595": ["AS_16"],
     },
 }
 
