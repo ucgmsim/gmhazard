9bd8fba64e641ef9525d7b86f30dcab4d4649206
sdw123123@gmail.com <sdw123123@gmail.com>
2021-11-26 11:31:28 +1300

    Update changelog and version for APIs

--- apis/core_api/setup.py
+++ apis/core_api/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="core_api",
-    version="20.11.3",
+    version="20.11.4",
     packages=find_packages(),
     url="https://github.com/ucgmsim/gmhazard",
     description="GMHazard Core API",
