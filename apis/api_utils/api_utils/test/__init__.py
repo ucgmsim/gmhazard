d43898828b7ddc12008654d7bacfd7bff15b9cb9
sdw123123@gmail.com <sdw123123@gmail.com>
2021-12-15 09:23:59 +1300

    tidy up the init

--- apis/api_utils/api_utils/test/__init__.py
+++ apis/api_utils/api_utils/test/__init__.py
@@ -1,1 +1,6 @@
-from .utils import send_test_request, response_checks, response_user_vs30_checks, MISSING_PARAM_MSG
+from .utils import (
+    send_test_request,
+    response_checks,
+    response_user_vs30_checks,
+    MISSING_PARAM_MSG,
+)
--- tools/gmhazard_utils/gmhazard_utils/__init__.py
+++ tools/gmhazard_utils/gmhazard_utils/__init__.py
@@ -1,12 +1,1 @@
-from .ensemble_creation import (
-    iterable_but_not_string,
-    flatten,
-    is_any_strings_in_string,
-    find_matching_dbs,
-    calculate_branch_weight,
-    read_gmm_weights,
-    create_ensemble,
-    get_branches,
-)
-
-from .utils import get_package_version, to_path, change_file_ext
+from . import utils, ensemble_creation
