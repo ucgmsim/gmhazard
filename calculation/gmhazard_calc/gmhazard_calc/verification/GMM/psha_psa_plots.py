eda044d3955009c7c41a36c4f78ddde0504e10f9
sdw123123@gmail.com <sdw123123@gmail.com>
2022-06-10 15:34:00 +1200

    minor tidy up

--- calculation/gmhazard_calc/gmhazard_calc/verification/GMM/psha_psa_plots.py
+++ calculation/gmhazard_calc/gmhazard_calc/verification/GMM/psha_psa_plots.py
@@ -1,4 +1,5 @@
 import pathlib
+import time
 from typing import List, Dict, Union
 
 import numpy as np
@@ -10,103 +11,69 @@ from empirical.util import empirical_factory
 from empirical.util.classdef import Site, Fault, TectType
 
 
-def init_setup(mag_dict: Dict, vs30_values: List):
-    """Create nested dictionaries for fault and result
-    based on a different vs30, mag and model
-
-    Parameters
-    ----------
-    mag_dict: Dict
-        List of Magnitudes
-    vs30_values: List
-        List of Vs30s
-    """
-    faults = {}
-    result_dict = {}
-    for tect_type, im_models in const.MODELS_DICT.items():
-        faults[tect_type] = {}
-        result_dict[tect_type] = {const.PSA_IM_NAME: {}}
-        for vs30 in vs30_values:
-            faults[tect_type][vs30] = {}
-            result_dict[tect_type][const.PSA_IM_NAME][vs30] = {}
-            for mag in mag_dict[tect_type]:
-                faults[tect_type][vs30][mag] = []
-                result_dict[tect_type][const.PSA_IM_NAME][vs30][mag] = {}
-                for model in im_models[const.PSA_IM_NAME]:
-                    result_dict[tect_type][const.PSA_IM_NAME][vs30][mag][model] = []
-
-    return faults, result_dict
-
-
-def get_sites(vs30_values: List, rrup_value: float):
+def get_sites(vs30_values: np.ndarray, rrup_value: float):
     """Creates a dictionary
     pair of a different Vs30 and Sites
 
     Parameters
     ----------
-    vs30_values: List
+    vs30_values: np.ndarray
         list of Vs30s
     rrup_value: float
         Rupture distance
     """
-    sites = {vs30: [] for vs30 in vs30_values}
-
-    for vs30 in vs30_values:
-        sites[vs30].append(
-            Site(
-                rrup=rrup_value,
-                rjb=rrup_value,
-                rx=rrup_value,
-                ry=rrup_value,
-                vs30=vs30,
-                **const.CONST_SITE_PARAMS,
-            )
+    return {
+        vs30: Site(
+            rrup=rrup_value,
+            rjb=rrup_value,
+            rx=rrup_value,
+            ry=rrup_value,
+            vs30=vs30,
+            **const.CONST_SITE_PARAMS,
         )
+        for vs30 in vs30_values
+    }
 
-    return sites
 
-
-def get_faults(vs30_values: List, mag_dict: Dict, faults: Dict):
+def get_faults(vs30_values: np.ndarray, mag_dict: Dict):
     """Creates a dictionary
      based on a different Vs30, Magnitude and Faults
 
     Parameters
     ----------
-    vs30_values: List
+    vs30_values: np.ndarray
         list of Vs30s
     mag_dict: Dict
         Dictionary with a different Mw lists for a different tectonic type
-    faults: Dict
-        nested dictionary
     """
+    faults = {}
     for tect_type in const.MODELS_DICT.keys():
+        faults[tect_type] = {}
         for vs30 in vs30_values:
+            faults[tect_type][vs30] = {}
             for mag in mag_dict[tect_type]:
-                faults[tect_type][vs30][mag].append(
-                    Fault(
-                        Mw=mag,
-                        tect_type=TectType[tect_type],
-                        **const.CONST_FAULT_PARAMS[tect_type],
-                    )
+                faults[tect_type][vs30][mag] = Fault(
+                    Mw=mag,
+                    tect_type=TectType[tect_type],
+                    **const.CONST_FAULT_PARAMS[tect_type],
                 )
 
     return faults
 
 
 def get_computed_gmms(
-    vs30_values: List,
+    vs30_values: np.ndarray,
     sites: Dict,
     mag_dict: Dict,
     faults: Dict,
-    result_dict: Dict,
-    period_values: List,
+    period_values: np.ndarray,
     for_sigma: bool,
 ):
     """Get computed GMMs
 
     Parameters
     ----------
-    vs30_values: List
+    vs30_values: np.ndarray
         list of Vs30s
     sites: Dict
         nested dictionary of Sites with a different Vs30
@@ -114,47 +81,46 @@ def get_computed_gmms(
         Dictionary with a different Mw lists for a different tectonic type
     faults: Dict
         nested dictionary of Faults with a different Vs30 and Magnitude
-    result_dict: Dict
-        nested dictionary with a different Vs30 and Magnitude
-    period_values: List
+    period_values: np.ndarray
         list of Periods
     for_sigma: bool
         to be used for pSA Sigma vs T
         if True
         else pSA vs T
     """
+    results_dict = {}
     for tect_type, im_models in const.MODELS_DICT.items():
+        results_dict[tect_type] = {}
+        results_dict[tect_type][const.PSA_IM_NAME] = {}
         for vs30 in vs30_values:
-            for site in sites[vs30]:
-                for mag in mag_dict[tect_type]:
-                    for fault in faults[tect_type][vs30][mag]:
-                        for model in im_models[const.PSA_IM_NAME]:
-                            results = empirical_factory.compute_gmm(
-                                fault,
-                                site,
-                                empirical_factory.GMM[model],
-                                const.PSA_IM_NAME,
-                                period_values,
-                            )
-                            if isinstance(results, list):
-                                result_dict[tect_type][const.PSA_IM_NAME][vs30][mag][
-                                    model
-                                ] = [
-                                    result[1][0] if for_sigma else result[0]
-                                    for result in results
-                                ]
-                            else:
-                                result_dict[tect_type][const.PSA_IM_NAME][vs30][mag][
-                                    model
-                                ] = [results[1][0] if for_sigma else results[0]]
+            results_dict[tect_type][const.PSA_IM_NAME][vs30] = {}
+            for mag in mag_dict[tect_type]:
+                results_dict[tect_type][const.PSA_IM_NAME][vs30][mag] = {}
+                for model in im_models[const.PSA_IM_NAME]:
+                    results = empirical_factory.compute_gmm(
+                        faults[tect_type][vs30][mag],
+                        sites[vs30],
+                        empirical_factory.GMM[model],
+                        const.PSA_IM_NAME,
+                        period_values,
+                    )
+                    if isinstance(results, list):
+                        results_dict[tect_type][const.PSA_IM_NAME][vs30][mag][model] = [
+                            result[1][0] if for_sigma else result[0]
+                            for result in results
+                        ]
+                    else:
+                        results_dict[tect_type][const.PSA_IM_NAME][vs30][mag][model] = [
+                            results[1][0] if for_sigma else results[0]
+                        ]
 
-    return result_dict
+    return results_dict
 
 
 def plot_psha_psa_sigma(
-    vs30_values: List,
+    vs30_values: np.ndarray,
     mag_dict: Dict,
-    period_values: List,
+    period_values: np.ndarray,
     result_dict: Dict,
     plot_directory: pathlib.PosixPath,
 ):
@@ -162,11 +128,11 @@ def plot_psha_psa_sigma(
 
     Parameters
     ----------
-    vs30_values: List
+    vs30_values: np.ndarray
         list of Vs30s
     mag_dict: Dict
         Dictionary with a different Mw lists for a different tectonic type
-    period_values: List
+    period_values: np.ndarray
         list of Periods
     result_dict: Dict
         nested dictionary with a different Vs30 and Magnitude
@@ -215,9 +181,9 @@ def plot_psha_psa_sigma(
 
 
 def plot_psha_psa(
-    vs30_values: List,
+    vs30_values: np.ndarray,
     mag_dict: Dict,
-    period_values: List,
+    period_values: np.ndarray,
     result_dict: Dict,
     plot_directory: pathlib.PosixPath,
 ):
@@ -225,11 +191,11 @@ def plot_psha_psa(
 
     Parameters
     ----------
-    vs30_values: List
+    vs30_values: np.ndarray
         list of Vs30s
     mag_dict: Dict
         Dictionary with a different Mw lists for a different tectonic type
-    period_values: List
+    period_values: np.ndarray
         list of Periods
     result_dict: Dict
         nested dictionary with a different Vs30 and Magnitude
@@ -329,28 +295,18 @@ def plot_psha_psa(
                 ax[x_position, y_position].yaxis.grid(
                     True, which="both", linestyle="dotted"
                 )
-                # If we want to save subplots individually
-                # plt.savefig(
-                #     f"{plot_directory}/{tect_type}_{str(mag).replace('.', 'p')}_{str(vs30).replace('.', 'p')}_pSA_versus_T.png"
-                # )
-                # plt.clf()
                 y_position += 1
             x_position += 1
 
-        # Uncomment those if we want to move the legend outside of grid
-        # Subplot will no longer holds legend
-        # fig.legend(loc=7)
         fig.tight_layout()
-        # fig.subplots_adjust(right=0.75)
-        # plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0)
         plt.savefig(f"{plot_directory}/{tect_type}_pSA_versus_T.png")
         plt.close()
 
 
 def plot_psha_median_psa(
-    vs30_values: List,
+    vs30_values: np.ndarray,
     mag_dict: Dict,
-    period_values: List,
+    period_values: np.ndarray,
     result_dict: Dict,
     plot_directory: pathlib.PosixPath,
 ):
@@ -358,11 +314,11 @@ def plot_psha_median_psa(
 
     Parameters
     ----------
-    vs30_values: List
+    vs30_values: np.ndarray
         list of Vs30s
     mag_dict: Dict
         Dictionary with a different Mw lists for a different tectonic type
-    period_values: List
+    period_values: np.ndarray
         list of Periods
     result_dict: Dict
         nested dictionary with a different Vs30 and Magnitude
@@ -440,9 +396,9 @@ def plot_psha_median_psa(
 
 
 def plot_psa_mag(
-    vs30_values: List,
-    rrup_values: List,
-    mag_list: List,
+    vs30_values: np.ndarray,
+    rrup_values: np.ndarray,
+    mag_list: np.ndarray,
     result_dict: Dict,
     plot_directory: pathlib.PosixPath,
     period,
@@ -483,28 +439,18 @@ def plot_psa_mag(
             ax[x_position, y_position].yaxis.grid(
                 True, which="both", linestyle="dotted"
             )
-            # If we want to save subplots individually
-            # plt.savefig(
-            #     f"{plot_directory}/{tect_type}_{str(mag).replace('.', 'p')}_{str(vs30).replace('.', 'p')}_pSA_versus_T.png"
-            # )
-            # plt.clf()
             y_position += 1
         x_position += 1
 
-    # Uncomment those if we want to move the legend outside of grid
-    # Subplot will no longer holds legend
-    # fig.legend()
     fig.tight_layout()
-    # fig.subplots_adjust(right=0.75)
-    # plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0)
     plt.savefig(f"{plot_directory}/{tect_type}_{period}.png")
     plt.close()
 
 
 def plot_psa_vs30(
-    vs30_values: List,
-    rrup_values: List,
-    mag_list: List,
+    vs30_values: np.ndarray,
+    rrup_values: np.ndarray,
+    mag_list: np.ndarray,
     result_dict: Dict,
     plot_directory: pathlib.PosixPath,
     tect_type,
@@ -547,19 +493,14 @@ def plot_psa_vs30(
             y_position += 1
         x_position += 1
 
-    # Uncomment those if we want to move the legend outside of grid
-    # Subplot will no longer holds legend
-    # fig.legend()
     fig.tight_layout()
-    # fig.subplots_adjust(right=0.75)
-    # plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0)
     plt.savefig(f"{plot_directory}/{tect_type}_{period if period else None}.png")
     plt.close()
 
 
 def psa_sigma_plot(
     mag_dict: Dict,
-    vs30_values: List,
+    vs30_values: np.ndarray,
     psa_periods: np.ndarray,
     rrup_values: List[Union[float, int]],
     save_path: pathlib.PosixPath = None,
@@ -579,15 +520,12 @@ def psa_sigma_plot(
     save_path: pathlib.PosixPath
         Directory to save plots
     """
-    faults, result_dict = init_setup(mag_dict, vs30_values)
-
+    faults = get_faults(vs30_values, mag_dict)
     for rrup in rrup_values:
         sites = get_sites(vs30_values, rrup)
 
-        faults = get_faults(vs30_values, mag_dict, faults)
-
         result_dict = get_computed_gmms(
-            vs30_values, sites, mag_dict, faults, result_dict, psa_periods, True
+            vs30_values, sites, mag_dict, faults, psa_periods, True
         )
 
         root_path = (
@@ -605,7 +543,7 @@ def psa_sigma_plot(
 
 def psa_plot(
     mag_dict: Dict,
-    vs30_values: List,
+    vs30_values: np.ndarray,
     psa_periods: np.ndarray,
     rrup_values: List[Union[float, int]],
     save_path: pathlib.PosixPath = None,
@@ -625,15 +563,12 @@ def psa_plot(
     save_path: pathlib.PosixPath
         Directory to save plots
     """
-    faults, result_dict = init_setup(mag_dict, vs30_values)
-
+    faults = get_faults(vs30_values, mag_dict)
     for rrup in rrup_values:
         sites = get_sites(vs30_values, rrup)
 
-        faults = get_faults(vs30_values, mag_dict, faults)
-
         result_dict = get_computed_gmms(
-            vs30_values, sites, mag_dict, faults, result_dict, psa_periods, False
+            vs30_values, sites, mag_dict, faults, psa_periods, False
         )
 
         root_path = (
@@ -650,7 +585,7 @@ def psa_plot(
 
 def psa_median_plot(
     mag_dict: Dict,
-    vs30_values: List,
+    vs30_values: np.ndarray,
     psa_periods: np.ndarray,
     rrup_value: List[Union[float, int]],
     save_path: pathlib.PosixPath = None,
@@ -670,15 +605,12 @@ def psa_median_plot(
     save_path: pathlib.PosixPath
         Directory to save plots
     """
-    faults, result_dict = init_setup(mag_dict, vs30_values)
-
+    faults = get_faults(vs30_values, mag_dict)
     for rrup in rrup_value:
         sites = get_sites(vs30_values, rrup)
 
-        faults = get_faults(vs30_values, mag_dict, faults)
-
         result_dict = get_computed_gmms(
-            vs30_values, sites, mag_dict, faults, result_dict, psa_periods, False
+            vs30_values, sites, mag_dict, faults, psa_periods, False
         )
 
         root_path = (
@@ -696,7 +628,7 @@ def psa_median_plot(
 
 def psa_mag_plot(
     mag_dict: Dict,
-    vs30_values: List,
+    vs30_values: np.ndarray,
     psa_periods: np.ndarray,
     rrup_values: List[Union[float, int]],
     save_path: pathlib.PosixPath = None,
@@ -915,27 +847,22 @@ if __name__ == "__main__":
         "SUBDUCTION_SLAB": [6, 7, 8],
         "SUBDUCTION_INTERFACE": [7, 8, 9],
     }
-    # vs30_list = [200, 300, 400, 760]
-    # vs30_list = [300, 760]
-    vs30_list = np.arange(100, 2001, 100)
-    # period_list = np.logspace(-2, 1, num=100)
-    # period_list =np.array([0.15, 0.17, 0.25])
+    vs30_list = np.array([200, 300, 400, 760])
     period_list = np.array([0.01, 0.1, 1.0, 2.0, 3.0, 5.0])
-    # rrup = [50, 100, 200, 300, 400, 500]
-    # rrup = [50]
-    rrup = [75, 200]
+    rrup = np.array([75, 200])
     # Update the path to the directory to save plots
-    # save_path = pathlib.Path("/home/tom/Documents/QuakeCoRE/resource/verification_plots")
-    # save_path = pathlib.Path("/home/tom/Documents/QuakeCoRE/resource/uhs_debug_data")
     save_path = pathlib.Path(
         "/home/tom/Documents/QuakeCoRE/resource/verification_plots/special"
     )
-
-    # psa_sigma_plot(mag_dict, vs30_list, period_list, rrup, save_path)
-    # psa_plot(mag_dict, vs30_list, period_list, rrup, save_path)
-    # psa_median_plot(mag_dict, vs30_list, period_list, rrup, save_path)
+    start = time.time()
+    psa_sigma_plot(mag_dict, vs30_list, period_list, rrup, save_path)
+    psa_plot(mag_dict, vs30_list, period_list, rrup, save_path)
+    psa_median_plot(mag_dict, vs30_list, period_list, rrup, save_path)
 
     # Special requests
-    # psa_mag_plot(mag_dict, vs30_list, period_list, rrup, save_path)
-    # psa_vs30_plot(mag_dict, vs30_list, period_list, rrup, save_path)
+    psa_mag_plot(mag_dict, vs30_list, period_list, rrup, save_path)
+    vs30_list = np.arange(100, 2001, 100)
+    psa_vs30_plot(mag_dict, vs30_list, period_list, rrup, save_path)
     pga_vs30_plot(mag_dict, vs30_list, rrup, save_path)
+
+    print(f"Finished in {(time.time() - start):.2f}s")
