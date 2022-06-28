104672ac94bbceb2a925fdcc8a6dfb066a9d8cee
sdw123123@gmail.com <sdw123123@gmail.com>
2022-06-28 16:21:53 +1200

    customized textarea looking div

--- frontend/src/components/common/MetadataBox.js
+++ frontend/src/components/common/MetadataBox.js
@@ -2,32 +2,55 @@ import React from "react";
 
 import * as CONSTANTS from "constants/Constants";
 
+import "assets/style/MetadataBox.css";
+
 const MetadataBox = ({ metadata }) => {
   const createMetadataText = () => {
-    let metadataText = "";
+    let metadataText = [];
+
     for (const [key, value] of Object.entries(metadata)) {
       if (key.includes("NZS1170.5 Soil")) {
-        metadataText += `${key}: ${CONSTANTS.NZS_SOIL_CLASS[value]}\n`;
+        metadataText.push(
+          <p key={key}>
+            {key}: {CONSTANTS.NZS_SOIL_CLASS[value]}
+          </p>
+        );
       } else if (key.includes("NZTA Soil")) {
-        metadataText += `${key}: ${CONSTANTS.NZTA_SOIL_CLASS[value]}\n`;
+        metadataText.push(
+          <p key={key}>
+            {key}: {CONSTANTS.NZTA_SOIL_CLASS[value]}
+          </p>
+        );
+      } else if (key.includes("Vs30")) {
+        metadataText.push(
+          <p key={key}>
+            V<sub>s30</sub>: {value}
+          </p>
+        );
+      } else if (key.includes("z1p0")) {
+        metadataText.push(
+          <p key={key}>
+            Z<sub>1.0</sub>: {value}
+          </p>
+        );
+      } else if (key.includes("z2p5")) {
+        metadataText.push(
+          <p key={key}>
+            Z<sub>2.5</sub>: {value}
+          </p>
+        );
       } else {
-        metadataText += `${key}: ${value}\n`;
+        metadataText.push(
+          <p key={key}>
+            {key}: {value}
+          </p>
+        );
       }
     }
 
-    return metadataText.slice(0, -1);
+    return metadataText;
   };
-  return (
-    <div className="form-group">
-      <textarea
-        style={{ color: "red", resize: "none" }}
-        className="form-control"
-        disabled
-        rows={Object.keys(metadata).length}
-        value={createMetadataText()}
-      ></textarea>
-    </div>
-  );
+  return <div className="custom-metadata-box">{createMetadataText()}</div>;
 };
 
 export default MetadataBox;
