08d89cf9d62928414cb85eac56b022c246740d2a
sdw123123@gmail.com <sdw123123@gmail.com>
2022-07-07 13:46:32 +1200

    switch to switch due to number of conditions

--- frontend/src/components/common/MetadataBox.js
+++ frontend/src/components/common/MetadataBox.js
@@ -9,46 +9,49 @@ const MetadataBox = ({ metadata }) => {
     let metadataText = [];
 
     for (const [key, value] of Object.entries(metadata)) {
-      if (
-        key.includes("NZS") &&
-        key.includes("1170.5") &&
-        key.includes("Soil")
-      ) {
-        metadataText.push(
-          <p key={key}>
-            {key}: {CONSTANTS.NZS_SOIL_CLASS[value]}
-          </p>
-        );
-      } else if (key.includes("NZTA") && key.includes("Soil")) {
-        metadataText.push(
-          <p key={key}>
-            {key}: {CONSTANTS.NZTA_SOIL_CLASS[value]}
-          </p>
-        );
-      } else if (key.includes("Vs30")) {
-        metadataText.push(
-          <p key={key}>
-            V<sub>s30</sub>: {value}
-          </p>
-        );
-      } else if (key.includes("z1p0")) {
-        metadataText.push(
-          <p key={key}>
-            Z<sub>1.0</sub>: {value}
-          </p>
-        );
-      } else if (key.includes("z2p5")) {
-        metadataText.push(
-          <p key={key}>
-            Z<sub>2.5</sub>: {value}
-          </p>
-        );
-      } else {
-        metadataText.push(
-          <p key={key}>
-            {key}: {value}
-          </p>
-        );
+      switch (key) {
+        default:
+          metadataText.push(
+            <p key={key}>
+              {key}: {value}
+            </p>
+          );
+          break;
+        case "NZS 1170.5 Soil Class":
+          metadataText.push(
+            <p key={key}>
+              {key}: {CONSTANTS.NZS_SOIL_CLASS[value]}
+            </p>
+          );
+          break;
+        case "NZTA Soil Class":
+          metadataText.push(
+            <p key={key}>
+              {key}: {CONSTANTS.NZTA_SOIL_CLASS[value]}
+            </p>
+          );
+          break;
+        case "Vs30":
+          metadataText.push(
+            <p key={key}>
+              V<sub>s30</sub>: {value}
+            </p>
+          );
+          break;
+        case "z1p0":
+          metadataText.push(
+            <p key={key}>
+              Z<sub>1.0</sub>: {value}
+            </p>
+          );
+          break;
+        case "z2p5":
+          metadataText.push(
+            <p key={key}>
+              Z<sub>2.5</sub>: {value}
+            </p>
+          );
+          break;
       }
     }
 
